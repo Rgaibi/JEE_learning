bde8f6364aaee2fcdb066310ec0621cff18794a2
karim rgaibi <karimrgaibi@gmail.com>
2025-08-23 17:59:29 +0200

    associer une vue à un servlet

--- src/main/java/com/apprendreJAVAEE/servlets/test.java
+++ src/main/java/com/apprendreJAVAEE/servlets/test.java
@@ -7,6 +7,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
+
 /**
  * Servlet implementation class test
  */
@@ -22,17 +24,12 @@ public class test extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		this.getServletContext().getRequestDispatcher("/WEB-INF/hello.jsp").forward(request, response);
+
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
---
+++ src/main/webapp/WEB-INF/hello.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>premier JSP</title>
+</head>
+<body>
+ <h1>hello </h1>
+ <p>voila une paragraphe</p>
+</body>
+</html>
+</body>
+</html>
